77882f433d961746933a6fefe2c55d7a8a9ef999
CM270 <107688639+CM270@users.noreply.github.com>
2023-03-04 18:19:13 +0100

    Update Variables.sol

--- exercices/ex02 - Variables/sources/Variables.sol
+++ exercices/ex02 - Variables/sources/Variables.sol
@@ -3,5 +3,9 @@
 pragma solidity ^0.8.4;
 
 contract Variables {
-
+function answerToLife = 42;
+function wrongAnswerToLife = -42;
+function sentence = "I'm a string";
+function isTrue = true;
+owner = 0x50d4EA6bb5F1aa93EE76D1b8a4091233BA8938C7;
 }
